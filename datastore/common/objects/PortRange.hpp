1aace4f18fa180129bc0409bca1c30d1aeb9348a
marshall-sg <62248261+marshall-sg@users.noreply.github.com>
2024-05-14 16:53:45 -0600

    updated to not be of type std::tuple

--- datastore/common/objects/PortRange.hpp
+++ datastore/common/objects/PortRange.hpp
@@ -31,17 +31,62 @@
 #include <string>
 #include <tuple>
 
+#include <netmeld/core/objects/AbstractObject.hpp>
+
+namespace nmco  = netmeld::core::objects;
 
 namespace netmeld::datastore::objects {
 
-  class PortRange : public std::tuple<uint16_t, uint16_t>
+  class Iterator {
+    private:
+      uint16_t value;
+
+    public:
+      // traits
+      using iterator_category = std::forward_iterator_tag;
+      using value_type        = uint16_t;
+      using difference_type   = std::ptrdiff_t;
+      using pointer           = const uint16_t*;
+      using reference         = const uint16_t&;
+
+      Iterator(uint16_t value) : value(value) {}
+
+      reference operator*() const { return value; }
+      pointer operator->() { return &value; }
+
+      Iterator& operator++() {
+        ++value;
+        return *this;
+      }
+      Iterator& operator++(int) {
+        Iterator tmp = *this;
+        ++(*this);
+        return tmp;
+      }
+      std::strong_ordering operator<=>(const Iterator& rhs) const {
+        return value <=> rhs.value;
+      }
+      bool operator==(const Iterator& rhs) const {
+        return 0 == operator<=>(rhs);
+      }
+  };
+
+  class PortRange : public nmco::AbstractObject
   {
     // =========================================================================
     // Variables
     // =========================================================================
     private:
     protected:
+      uint16_t min;
+      uint16_t max;
+
     public:
+      const uint16_t& first;
+      const uint16_t& last;
+
+      Iterator begin() const {return Iterator(min); }
+      Iterator end()   const {return Iterator(max); }
 
     // =========================================================================
     // Constructors
@@ -49,6 +94,7 @@ namespace netmeld::datastore::objects {
     private:
     protected:
     public:
+      explicit PortRange();
       explicit PortRange(uint16_t);
       explicit PortRange(uint16_t, uint16_t);
       explicit PortRange(const std::string&);
@@ -58,13 +104,17 @@ namespace netmeld::datastore::objects {
     // =========================================================================
     private:
       std::string translateFromTypicalServiceAlias(const std::string&) const;
+
     protected:
     public:
       std::string toString() const;
       std::string toDebugString() const;
       std::string toHumanString() const;
 
-      friend std::ostream& operator<<(std::ostream&, const PortRange&);
+      std::strong_ordering operator<=>(const PortRange&) const;
+      bool operator==(const PortRange&) const;
+
+      PortRange& operator=(const PortRange&);
   };
 }
 #endif // PORTRANGE_HPP
