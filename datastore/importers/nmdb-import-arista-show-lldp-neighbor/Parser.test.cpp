f864e912d8ee914a69a6ae2744b518da5413a77d
Raul Cruz <raulcruz12345@gmail.com>
2024-08-27 22:46:59 -0400

    added unit tests by parts

--- datastore/importers/nmdb-import-arista-show-lldp-neighbor/Parser.test.cpp
+++ datastore/importers/nmdb-import-arista-show-lldp-neighbor/Parser.test.cpp
@@ -38,12 +38,311 @@ using qi::ascii::blank;
 
 class TestParser : public Parser {
   public:
+    using Parser::nd;
+
     using Parser::start;
+
+    using Parser::noDetailTableInfo;
+    using Parser::noDetailHeader;
+    using Parser::noDetailEntry;
+
+    using Parser::detailHeader;
+    using Parser::detailNeighborLine;
+    using Parser::detailDiscovered;
+    using Parser::detailChassisId;
+    using Parser::detailPortId;
+    using Parser::detailPortDescription;
+    using Parser::detailSystemName;
+    using Parser::detailSystemDescription;
+    using Parser::detailCapabilities;
 };
+
+BOOST_AUTO_TEST_CASE(testNoDetailParts)
+{
+  TestParser tp;
+
+  {
+    tp.nd = NeighborData();
+    const auto& parserRule {tp.noDetailTableInfo};
+
+    std::vector<std::string> testsOk {
+        "Last table change time   : 0:00:16 ago\n"
+        "Number of table inserts  : 11\n"
+        "Number of table deletes  : 5\n"
+        "Number of table drops    : 0\n"
+        "Number of table age-outs : 0\n"
+      , "Last table change time   : 23:59:59 ago\n"
+        "Number of table inserts  : 11\n"
+        "Number of table deletes  : 500\n"
+        "Number of table drops    : 100\n"
+        "Number of table age-outs : 100\n"
+        "\n"
+      };
+    for (const auto& test : testsOk) {
+      BOOST_TEST( nmdp::test(test.c_str(), parserRule, blank)
+                , "Parser rule 'noDetailTableInfo': " << test
+                );
+      BOOST_TEST(NeighborData() == tp.nd);
+    }
+  }
+
+  {
+    tp.nd = NeighborData();
+    const auto& parserRule {tp.noDetailHeader};
+
+    std::vector<std::string> testsOk {
+        "Port  Neighbor Device ID  Neighbor Port ID  TTL\n"
+        "-----------------------------------------------\n"
+      , "Port\tNeighbor Device ID\tNeighbor Port ID\tTTL\n"
+        "-----------------------------------------------\n"
+      };
+    for (const auto& test : testsOk) {
+      BOOST_TEST( nmdp::test(test.c_str(), parserRule, blank)
+                , "Parser rule 'noDetailHeader': " << test
+                );
+      BOOST_TEST(NeighborData() == tp.nd);
+    }
+  }
+
+  {
+    const auto& parserRule {tp.noDetailEntry};
+
+    std::vector<std::tuple<std::string, std::string, std::string>> testsOk {
+        {"Et1  HOST1.domain  Ethernet1  120\n"
+        , "HOST1.domain", "Ethernet1"
+        }
+      , {"Et2\tHOST2.domain\tEthernet2\t120\n"
+        , "HOST2.domain", "Ethernet2"
+        }
+      , {"Et3  host3.domain  Ethernet3  120\n"
+        , "host3.domain", "Ethernet3"
+        }
+      , {"Et4\thost4.domain\tEthernet4\t120\n"
+        , "host4.domain", "Ethernet4"
+        }
+      , {"Et5/1  HOST5.domain  Ethernet5  120\n"
+        , "HOST5.domain", "Ethernet5"
+        }
+      , {"Et6/1\tHOST6.domain\tEthernet6\t120\n"
+        , "HOST6.domain", "Ethernet6"
+        }
+      , {"Et7  HOST7.domain  Ethernet 7/1  120\n"
+        , "HOST7.domain", "Ethernet 7/1"
+        }
+      , {"Et8\tHOST8.domain\tEthernet 8/1\t120\n"
+        , "HOST8.domain", "Ethernet8/1"
+        }
+      , {"Et9  1234.5678.90ab  Ethernet9  120\n"
+        , "1234.5678.90ab", "Ethernet9"
+        }
+      , {"Et10\t1234.5678.90ab\tEthernet10\t120\n"
+        , "1234.5678.90ab", "Ethernet10"
+        }
+      , {"Et11  HOST1.domain  net11-2  120\n"
+        , "HOST11.domain", "Ethernet11"
+        }
+      , {"Et12\tHOST12.domain\tnet12-2\t120\n"
+        , "HOST12.domain", "net12-2"
+        }
+      };
+    for (const auto& [test, hostName, ifaceName] : testsOk) {
+      tp.nd = NeighborData();
+      BOOST_TEST( nmdp::test(test.c_str(), parserRule, blank)
+                , "Parse rule 'noDetailEntry': " << test
+                );
+      BOOST_TEST(hostName == tp.nd.curHostname);
+      BOOST_TEST(ifaceName == tp.nd.curIfaceName, '|' + ifaceName + "| |" + tp.nd.curIfaceName + '|');
+    }
+  }
+}
+
+BOOST_AUTO_TEST_CASE(testDetailParts)
+{
+  TestParser tp;
+
+  {
+    tp.nd = NeighborData();
+    const auto& parserRule {tp.detailHeader};
+
+    std::vector<std::string> testsOk {
+        "Interface Ethernet 4/1 detected 1 LLDP neighbors:\n\n"
+      , "Interface Ethernet4/1 detected 1 LLDP neighbors:\n\n"
+      , "Interface Ethernet 4 detected 1 LLDP neighbors:\n\n"
+      , "Interface Ethernet4 detected 1 LLDP neighbors:\n\n"
+      };
+    for (const auto& test : testsOk) {
+      BOOST_TEST( nmdp::test(test.c_str(), parserRule, blank)
+                , "Parse rule 'detailHeader': " << test
+                );
+      BOOST_TEST(NeighborData() == tp.nd);
+    }
+  }
+
+  {
+    tp.nd = NeighborData();
+    const auto& parserRule {tp.detailNeighborLine};
+
+    std::vector<std::string> testsOk {
+        "\tNeighbor 1234.5678.90ab/\"123\", age 1 seconds\n"
+      , "\tNeighbor 1234.5678.90ab/\"Ethernet6\", age 4 seconds\n"
+      , "\tNeighbor 1234.5678.90ab/\"Ethernet6/25\", age 7 seconds\n"
+      , "\tNeighbor 1234.5678.90ab/\"Ethernet 6/25\", age 10 seconds\n"
+      , "\tNeighbor 1234.5678.90ab/Ethernet6, age 13 seconds\n"
+      , "\tNeighbor 1234.5678.90ab/Ethernet6/25, age 16 seconds\n"
+      , "\tNeighbor 1234.5678.90ab/Ethernet 6/25, age 19 seconds\n"
+      };
+    for (const auto& test : testsOk) {
+      BOOST_TEST( nmdp::test(test.c_str(), parserRule, blank)
+                , "Parse rule 'detailNeighborLine': " << test
+                );
+      BOOST_TEST(NeighborData() == tp.nd);
+    }
+  }
+
+  {
+    tp.nd = NeighborData();
+    const auto& parserRule {tp.detailDiscovered};
+
+    std::vector<std::string> testsOk {
+        "\tDiscovered 5 days, 3:58:58 ago; Last changed 5 days, 0:00:01 ago\n"
+      , "\tDiscovered 365 days, 00:07:18 ago; Last changed 38 days, 18:07:16 ago\n"
+      };
+    for (const auto& test : testsOk) {
+      BOOST_TEST( nmdp::test(test.c_str(), parserRule, blank)
+                , "Parse rule 'detailDiscovered': " << test
+                );
+      BOOST_TEST(NeighborData() == tp.nd);
+    }
+  }
+
+  {
+    const auto& parserRule {tp.detailChassisId};
+
+    std::vector<std::tuple<std::string,std::string>> testsOk {
+        {"- Chassis ID type: MAC address (4)\nChassis ID     : 1234.5678.90ab\n"
+        , "1234.5678.90ab"
+        }
+      , {"- Chassis ID type: MAC address (4)\nChassis ID     : 001c.730f.11a8qqq\n"
+        , "001c.730f.11a8qqq"
+        }
+      };
+    for (const auto& [test, chassisId] : testsOk) {
+      tp.nd = NeighborData();
+      BOOST_TEST( nmdp::test(test.c_str(), parserRule, blank)
+                , "Parse rule 'detailChassisId': " << test
+                );
+      BOOST_TEST(chassisId == tp.nd.curMacAddr);
+    }
+  }
+
+  {
+    const auto& parserRule {tp.detailPortId};
+
+    std::vector<std::tuple<std::string,std::string>> testsOk {
+        {"- Port ID type: Interface name(1)\nPort ID     : \"Ethernet1\"\n"
+        , "Ethernet1"
+        }
+      , {"- Port ID type: Interface name (5)\nPort ID     : \"Ethernet6/25\"\n"
+        , "Ethernet6/25"
+        }
+      };
+    for (const auto& [test, portId] : testsOk) {
+      tp.nd = NeighborData();
+      BOOST_TEST( nmdp::test(test.c_str(), parserRule, blank)
+                , "Parse rule 'detailPortId': " << test
+                );
+      BOOST_TEST(portId == tp.nd.curIfaceName);
+    }
+  }
+
+  {
+    const auto& parserRule {tp.detailPortDescription};
+
+    std::vector<std::tuple<std::string,std::string>> testsOk {
+        {"  - Port Description: \"HOST_Et1\"\n"
+        , "HOST_Et1"
+        }
+      , {"- Port Description: \"\"\n"
+        , ""
+        }
+      };
+    for (const auto& [test, portDescription] : testsOk) {
+      tp.nd = NeighborData();
+      BOOST_TEST( nmdp::test(test.c_str(), parserRule, blank)
+                , "Parse rule 'detailPortDescription': " << test
+                );
+      BOOST_TEST(portDescription == tp.nd.curPortDescription);
+    }
+  }
+
+  {
+    const auto& parserRule {tp.detailSystemName};
+
+    std::vector<std::tuple<std::string,std::string>> testsOk {
+        {"- System Name: \"HOST.domain\"\n"
+        , "HOST.domain"
+        }
+      , {"- System Name: \"host1.domain\"\n"
+        , "host1.domain"
+        }
+      , {"- System Name: \"web.site.com\"\n"
+        , "web.site.com"
+        }
+      };
+    for (const auto& [test, systemName] : testsOk) {
+      tp.nd = NeighborData();
+      BOOST_TEST( nmdp::test(test.c_str(), parserRule, blank)
+                , "Parse rule 'detailSystemName': " << test
+                );
+      BOOST_TEST(systemName == tp.nd.curHostname);
+    }
+  }
+
+  {
+    const auto& parserRule {tp.detailSystemDescription};
+
+    std::vector<std::tuple<std::string,std::string>> testsOk {
+        {"- System Description: \"Arista Networks EOS version 4.26.5M running on an Arista Networks DCS-7280SR-48C6\"\n"
+        , "Arista Networks EOS version 4.26.5M running on an Arista Networks DCS-7280SR-48C6"
+        }
+      , {"- System Description: \"\"\n"
+        , ""
+        }
+      };
+    for (const auto& [test, systemDescription] : testsOk) {
+      tp.nd = NeighborData();
+      BOOST_TEST( nmdp::test(test.c_str(), parserRule, blank)
+                , "Parse rule 'detailSystemDescription': " << test
+                );
+      BOOST_TEST(systemDescription == tp.nd.curSysDescription);
+    }
+  }
+
+  {
+    const auto& parserRule {tp.detailCapabilities};
+
+    std::vector<std::tuple<std::string,std::string>> testsOk {
+        {"- System Capabilities : Bridge, Router\nEnabled Capabilities: Bridge, Router\n"
+        , "Bridge, Router"
+        }
+      , {"- System Capabilities : Bridge, Router\nEnabled Capabilities: Bridge\n"
+    //  , "Bridge"
+        , "Bridge, Router"
+        }
+      };
+    for (const auto& [test, capabilities] : testsOk) {
+      tp.nd = NeighborData();
+      BOOST_TEST( nmdp::test(test.c_str(), parserRule, blank)
+                , "Parse rule 'detailCapabilities': " << test
+                );
+      BOOST_TEST(capabilities == tp.nd.curDeviceType);
+    }
+  }
+}
 
 BOOST_AUTO_TEST_CASE(testWholeNonDetail)
 {
-  { // IOS
+  {
     TestParser tp;
     const auto& parserRule {tp.start};
 
