e77cb46f9db59b7956cebced18ba3dcc477374bf
Raul Cruz <raulcruz12345@gmail.com>
2024-09-19 13:19:35 -0400

    add expected fail test for 'detailHeader'

--- datastore/importers/nmdb-import-arista-show-lldp-neighbor/Parser.test.cpp
+++ datastore/importers/nmdb-import-arista-show-lldp-neighbor/Parser.test.cpp
@@ -211,6 +211,16 @@ BOOST_AUTO_TEST_CASE(testDetailParts)
                 );
       BOOST_TEST(srcIfaceName == tp.nd.srcIfaceName);
     }
+
+    std::vector<std::string> testsFail {
+        "    Interface Number          : 1234567\n"
+      };
+    for (const auto& test : testsFail) {
+      BOOST_TEST( !nmdp::test(test.c_str(), parserRule, blank)
+                , "Parse rule 'detailHeader': " << test
+                );
+      BOOST_TEST(NeighborData() == tp.nd);
+    }
   }
 
   {
