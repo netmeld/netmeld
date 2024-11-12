e6c85edf3814405bb9cd52075c40275c336b31e0
Raul Cruz <raulcruz12345@gmail.com>
2024-11-11 20:59:55 -0500

    modify to catch if somebody incorporates interface name expansion

--- datastore/common/objects/Vrf.test.cpp
+++ datastore/common/objects/Vrf.test.cpp
@@ -70,10 +70,10 @@ BOOST_AUTO_TEST_CASE(testSetters)
   // Setters
   vrf.setId(id);
   vrf.addIface("eth0");
-  vrf.addIface("eth1");
+  vrf.addIface("gi0");
 
   BOOST_CHECK_EQUAL(id, vrf.getId());
   BOOST_CHECK_EQUAL(2, vrf.getIfaces().size());
   BOOST_CHECK_EQUAL("eth0", vrf.getIfaces().at(0).getName());
-  BOOST_CHECK_EQUAL("eth1", vrf.getIfaces().at(1).getName());
+  BOOST_CHECK_EQUAL("gi0", vrf.getIfaces().at(1).getName());
 }
